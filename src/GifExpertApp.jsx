15b0e14f8f74c94f85d2e81ec5da5de4a9793469
Ramiro Fassetta <ramiro.fassetta.01@gmail.com>
2023-06-05 23:42:34 -0300

    first commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,33 @@
+import {useState} from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(["One Punch"]);
+
+  const onAddCategory = (newCategory) => {
+    if(categories.includes(newCategory)) return;
+
+    // console.log(newCategory);
+    setCategories([newCategory,...categories]);
+  }
+
+  return (
+  <>
+    
+    <h1>GifExpertApp</h1>
+
+    <AddCategory 
+        // setCategories={ setCategories } 
+        onNewCategory={ event => onAddCategory(event) }
+    />
+    
+      {categories.map( (category) => (
+      <GifGrid key={category} category={category}/>
+      ))
+      }
+
+  </>
+  )
+}
